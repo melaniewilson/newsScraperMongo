578fc75b88264fb376fff0cd28fb8a25f4378246
melanie <melaniew1357@gmail.com>
2017-12-11 16:41:20 -0800

    small change

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ var routes = require("./controllers/scraper_controller.js");
 
 app.use("/", routes);
 
-mongoose.connect();
+mongoose.connect("mongodb://heroku_gnzk5747:4d2121nhgnfbdl1pfirsdepk9n@ds125262.mlab.com:25262/heroku_gnzk5747");
 
 var db = mongoose.connection;
 
